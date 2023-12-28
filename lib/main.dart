59efe088be7c792e7ab04b96c12b56f1e4a48ce4
Aravindraj2k04 <117381999+Aravindraj2k04@users.noreply.github.com>
2023-12-28 21:14:52 +0530

    .

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,9 @@ void main() {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color.fromARGB(255, 48, 106, 240),
-        
+
         appBar: AppBar(
+          
           title: Text('Dicee'),
           backgroundColor: Color.fromARGB(255, 48, 106, 240),
         ),
